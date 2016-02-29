df858f4b6305e7db180a7ed3292e26830c9e0a86
Ina Tsetsova <inatsetsova@gmail.com>
2016-02-29 16:58:43 +0000

    adds a prompt user to fill out a form test

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,6 +8,7 @@ feature 'restaurants' do
 			expect(page).to have_link 'Add a restaurant'
 		end
 	end
+
 	context 'restaurants have been added' do
 		before do
 			Restaurant.create(name: 'KFC')
@@ -17,6 +18,17 @@ feature 'restaurants' do
 			visit '/restaurants'
 			expect(page).to have_content('KFC')
 			expect(page).not_to have_content('No restaurant yet!')
+		end
+	end
+
+	context 'creating restaurants' do
+		scenario 'prompts user to fill out a form, then displays the new restaurant' do
+			visit '/restaurants'
+			click_link 'Add a restaurant'
+			fill_in 'Name', with: 'KFC'
+			click_button 'Create Restaurant'
+			expect(page).to have_content 'KFC'
+			expect(current_path).to eq '/restaurants'
 		end
 	end
 end
