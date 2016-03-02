4516c6fbc76d05c11e061b83fee2301841f2a256
Ina Tsetsova <inatsetsova@gmail.com>
2016-03-02 18:24:10 +0000

    facebook authentication still not working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,10 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret,
+                callback_url: "http://localhost:3000"
+
+  
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
